344983a40c0df31b02d56b92a80f7a4885f96d9c
Igor Parfenov <lloopy123123543@gmail.com>
2023-05-06 21:47:43 +0500

    main menu

---
+++ pro_vite/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <div class="container mx-auto flex">
+      <header class="flex text-xl w-full justify-between pt-10">
+        <div class="text-rose-800 font-bold">LOGO</div>
+        <div class="lg:flex lg:space-x-5 hidden">
+          <div>Калькулятор</div>
+          <div>Монтаж окон</div>
+          <div>Контакты</div>
+          <div>Личный кабинет</div>
+        </div>
+        <div class="flex lg:hidden">
+          <div class="">
+            <div>Калькулятор</div>
+            <div>Монтаж окон</div>
+            <div>Контакты</div>
+            <div>Личный кабинет</div>
+          </div>
+        </div>
+      </header>
+    </div>
+    <HelloWorld />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pro_vite/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div w-full>
+    <div class="container mx-auto">
+      <div class="w-full bg-slate-600 p-10 space-y-3">
+        <div class="text-Main text-xl flex items-center ">lineSubtitle</div>
+        <div class="text-white text-8xl font-bold">TITLE</div>
+        <div class="text-white lg:w-1/2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates error ipsum
+          quas dolorum est minima, praesentium omnis cupiditate pariatur rerum.</div>
+        <button class="px-10 py-2 bg-Main">Смотреть</button>
+      </div>
+    </div>
+    <div class="container mx-auto flex justify-center">
+      <div class="w-5/6 bg-white grid grid-cols-3">
+        <div class="bg-Main">
+          <div class="text-3xl">Title</div>
+          <div>BigTitle</div>
+          <div class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis ipsum laudantium eius rem
+            temporibus, quia quisquam, obcaecati </div>
+          <button>WorkFlow</button>
+        </div>
+        <div>
+          <div class="flex justify-center items-center h-1/2 p-10 min-h-16">
+            <div><img src="../assets/vue.svg" alt=""></div>
+            <div>
+              <div class="text-5xl font-bold">Title</div>
+              <div class="text-gray-500">Subtitle</div>
+            </div>
+          </div>
+          
+        </div>
+        <div>ssss</div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
